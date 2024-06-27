928995cf2787efdee110a81ae2b1c3cf73bb48a4
abhishek <dskumar021@gmail.com>
2024-06-27 18:57:47 +0530

    completed data transformation pipeline

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,62 @@
+from src.constant import *
+
+from src.config.configuration import *
+
+import os,sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from src.logger import logging
+from src.exception import CustomException
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
+# Define configuration (1)  :define raw data pah/train file path / test file path 
+class DataIngestionconfig:
+
+    raw_data_path:str = RAW_FILE_PATH
+    train_data_path :str =  TRAIN_FILE_PATH
+    test_data_path:str  = TEST_FILE_PATH
+        
+
+class DataIngestion:
+    def __init__(self):
+        self.data_ingestion_config = DataIngestionconfig()
+
+
+    def initiate_data_ingestion(self):
+        try:
+            # 1 read dataset 
+            df=pd.read_csv(DATASET_PATH)
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.raw_data_path),exist_ok =True)
+            #2 save to raw_data_path
+            df.to_csv(self.data_ingestion_config.raw_data_path ,index =False)
+
+            #3 split into train and test
+            train_set ,test_set =train_test_split(df ,test_size=0.20 ,random_state =42)
+
+            #4 afer split data and save into traindatapath and testdatapath 
+            os.makedirs(os.path.dirname(self.data_ingestion_config.train_data_path ),exist_ok =True)
+            train_set.to_csv(self.data_ingestion_config.train_data_path)
+
+
+
+            os.makedirs(os.path.dirname(self.data_ingestion_config.test_data_path ),exist_ok =True)
+            test_set.to_csv(self.data_ingestion_config.test_data_path)
+
+            return(
+                self.data_ingestion_config.train_data_path,
+                self.data_ingestion_config.test_data_path
+            )
+        
+        except Exception as e:
+            raise CustomException( e, sys)
+
+if __name__ =='__main__':
+    obj = DataIngestion()
+    train_data_path ,test_data_path=obj.initiate_data_ingestion()
+
+
+    data_transformation =DataTransformation()
+    #train_arr and test_arr calling hee
+    train_arr ,test_arr = data_transformation.inititate_data_transformation(train_data_path ,test_data_path)
---
+++ src/constant/__init__.py
@@ -0,0 +1,43 @@
+import os ,sys
+from datetime import datetime
+
+# artifact --> pipeline folder--> output
+
+
+def get_current_time_stamp():
+    return f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+
+CURRENT_TIME_STAMP = get_current_time_stamp()
+
+ROOT_DIR_KEY =  os.getcwd()
+DATA_DIR ='Data'
+DATA_DIR_KEY ="finalTrain.csv"
+
+
+ARTIFACT_DIR_KEY = "Artifact"
+
+# Data ingestion related variable
+
+DATA_INGESTION_KEY = "data_ingestion"
+DATA_INGESTION_RAW_DATA_DIR= "raw_data_dir"
+DATA_INGESTION_INGESTED_DATA_DIR_KEY ='ingested_dir'
+
+RAW_DATA_DIR_KEY= "raw.csv"
+TRAIN_DATA_DIR_KEY= "train.csv"
+TEST_DATA_DIR_KEY = "test.csv"
+
+
+
+
+############################################
+# data transformation 
+DATA_TRANSFORMATION_ARTIFACT = "data_transformation"
+DATA_PREPROCCED_DIR = "processor"
+
+DATA_TRANSFORMATION_PROCESSING_OBJ= 'processor.pkl'
+DATA_TRANSFORM_DIR = 'transformation'
+TRANSFORM_TRAIN_DIR_KEY = 'train.csv'
+TRANSFORM_TEST_DIR_KEY= 'test.csv'
+#artifact--> data_transformation / processor -> processor.pkl and transofmration-> train.csv  and test.csv
+
+
